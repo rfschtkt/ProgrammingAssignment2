807e3d2d6c5a6414c91dd369694d4e8fd5eba6da
Raf Schietekat <undisclosed@address>
2014-07-27 11:51:55 -0700

    Variant

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## This mimics the makeVector() function in the example.
-## It provides a simple bundle of data and get/set operations,
-## but nothing really interesting yet.
+## This is a bit more like an object.
 
 makeCacheMatrix <- function(x = matrix()) {
   m_inverse <- NULL # the cached inverse
-  set <- function(y) {
-    x <<- y
-    m_inverse <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(a_inverse) m_inverse <<- a_inverse
-  getinverse <- function() m_inverse
   list(
-    set = set,
-    get = get,
-    setinverse = setinverse,
-    getinverse = getinverse
+    set = function(y) {
+      x <<- y
+      m_inverse <<- NULL
+    },
+    get = function() x,
+    getinverse = function() {
+      if(!is.null(m_inverse)) {
+        message("getting cached inverse")
+        return(m_inverse)
+      }
+      m_inverse <<- solve(x)
+      m_inverse
+    }
     )
-}
-
-
-## Mysteriously external operation that returns the inverse or its cached value.
-
-cacheSolve <- function(x, ...) {
-  inverse <- x$getinverse()
-  if(!is.null(inverse)) {
-    message("getting cached inverse")
-    return(inverse)
-  }
-  data <- x$get()
-  inverse <- solve(data)
-  x$setinverse(inverse)
-  inverse
 }
